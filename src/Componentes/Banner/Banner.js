0ff807e26aed85c722cf6ca88a633a2dd9d41159
Eder Cruz <edercruz.x@gmail.com>
2022-07-27 10:45:06 -0300

    Eder Commits

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from "react";
+import   "./Banner.css"
+
+export const  Banner =  () => {
+return (
+    <header className="banner"> 
+<img  src="/images/banner.png"  alt="banner do header"/>
+</header>
+
+)
+
+
+
+}
